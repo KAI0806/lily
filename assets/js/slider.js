f63821c7d3ef7d297b6c32892a599d11b5f4eb05
甲斐聖健 <>
2022-02-14 19:45:04 +0900

    ハンバーガーまで完成

---
+++ assets/js/slider.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 2,
+    pauseOnFocus: false,//フォーカスで一時停止
+    pauseOnHover: false,//マウスホバーで一時停止
+    pauseOnDotsHover: false,
+    dots:true // インジケーターを表示
+  });
